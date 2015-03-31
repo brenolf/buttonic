ada19a73e3a7dddf20839829c668ad4126bf74c6
Breno Lima de Freitas <brenolimadefreitas@gmail.com>
2015-03-30 23:17:55 -0300

    Deploy on gh-pages

--- js/js.js
+++
@@ -1,7 +0,0 @@
-$('div[id^="example"]').each(function(){
-	var o = $(this);
-	var code = o.html().replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/\t/g, '');
-	o.after('<pre class="prettyprint linenums" id="flat-code">' + code + '</pre>');
-});
-
-$('#css-rules').html($('#css-rules').html().replace(/\t/g, ''));
